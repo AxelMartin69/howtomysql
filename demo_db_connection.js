b6f115e6abfd51c64276d1b5e5de35e8ed0efba0
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-02-07 16:46:27 +0200

    Tabeli loomine

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -3,14 +3,16 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "qwerty"
+    password: "qwerty",
+    database: "mydb"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
+    var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+    con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Database created");
+        console.log("Table created");
     });
 });
